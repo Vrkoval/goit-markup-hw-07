8aae7515c0251deeedef05666b3c05070d51960e
Viktoriya Koval <vrkoval@gmail.com>
2023-01-04 22:15:36 +0200

    mobile.menu

---
+++ js/mobile-menu.js
@@ -0,0 +1,23 @@
+(() => {
+	const refs = {
+		openMenuBtn: document.querySelector('[data-menu-open]'),
+		closeMenuBtn: document.querySelector('[data-menu-close]'),
+		menu: document.querySelector('[data-menu]'),
+	};
+	refs.openMenuBtn.addEventListener("click", toggleMenu);
+	refs.closeMenuBtn.addEventListener("click", toggleMenu);
+
+	function toggleMenu() {
+		document.body.classList.toggle("menu-open");
+		refs.menu.classList.toggle("is-open");
+	}
+})();
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
